b53608cb3e4909011ecaf625cbae743415eea6f2
tsidk29 <tsidk29@gmail.com>
2022-01-09 09:30:16 -0800

    python-network

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes in a URL, sends a GET request to URL and displays the body of response
+curl -sL "$1"
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+fetch X-Request-Id from header response with url
+"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        content = response.getheader('X-Request-Id')
+    print(content)
